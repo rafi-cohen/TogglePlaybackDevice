a75eafb150a0510f2740c8920287c1031c4b8a4e
Rafi Cohen <77580049+rafi-cohen@users.noreply.github.com>
2021-10-30 16:00:47 +0300

    Use AudioDevice.ID instead of AudioDevice.Index

--- TogglePlaybackDevice.ps1
+++ TogglePlaybackDevice.ps1
@@ -1,6 +1,6 @@
 $playbackDevices = Get-AudioDevice -List | Where-Object {$_.Type -eq 'Playback'}
 $currentDevice = Get-AudioDevice -Playback
-$currentDeviceIndex = $playbackDevices.ForEach{$_.Index}.IndexOf($currentDevice.Index)
+$currentDeviceIndex = $playbackDevices.ForEach{$_.ID}.IndexOf($currentDevice.ID)
 $nextDeviceIndex = ($currentDeviceIndex + 1) % $playbackDevices.Length
 $nextDevice = $playbackDevices[$nextDeviceIndex]
-Set-AudioDevice -Index $nextDevice.Index
+Set-AudioDevice -ID $nextDevice.ID
